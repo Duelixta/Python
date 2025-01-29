45f6d196f4c42c31486a58f7a0356031cbe67736
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-01-29 11:10:38 -0300

    Create Variaveis1.py

---
+++ Variaveis1.py
@@ -0,0 +1,6 @@
+#toda função tem parenteses, e todos comandos são funções.
+print("Hello World")
+print(7+4)
+print('7'+ '4')
+print('Olá', 5)
+#Todos esses prints tem diferenças, como por exemplo o primeiro print que é uma string, o segundo é uma soma, o terceiro é uma concatenação e o quarto é uma concatenação de uma string com um número.
