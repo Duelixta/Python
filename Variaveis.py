a3ff63da1f52f005eea2d9063b3774c6017d04d6
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-01-29 16:02:30 -0300

    Variaveis

---
+++ Variaveis.py
@@ -0,0 +1,43 @@
+#Expressões são operações na qual a linguagem realiza, como por exemplo a soma, a subtração, a multiplicação e a divisão.
+#Strings são textos, e podem ser concatenadas, ou seja, juntadas.
+#Variáveis são valores que podem ser armazenados e reutilizados.
+#Variáveis podem ser declaradas e inicializadas.
+#Variáveis podem ser reutilizadas.
+#Variáveis podem ser atualizadas.
+
+#exemplos
+
+25/5 = 5
+25/6 = 4.166666666666667
+
+#Barra dupla o resultado é arrendodado para baixo.
+25//5 = 5
+25//6 = 4
+
+#multiplicação sempre vem primeiro
+
+2*60+30 = 150
+
+30+2*60 = 150
+
+#Os parenteses são executados primeiro.
+
+(30+2)*60 = 1920
+
+#Variáveis
+
+my_variable = 1
+my_variable:1
+print(my_variable)
+my_variable = 2 #O valor antigo nao importa, o novo valor é 2
+x = 43 + 60 + 16 + 41 #Variaveis podem ser resultado de expressões
+x:160
+y=x/60
+y= 2.6666666666666665
+x=x/60
+x:2.6666666666666665
+
+#Pequeno problema, converter minutos de determinadas musicas
+
+minutostotais = 43 + 42 + 57
+horastotais = minutostotais/60
