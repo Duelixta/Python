8b56f94d812b7f6b7be4e14fde75fd0eb2fbbe03
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-01-30 01:23:02 -0300

    Atividade 3

---
+++ Atividade3.py
@@ -0,0 +1,9 @@
+#Dia 30/01/2025 atividade numero 3
+#Crie um script que leia dois números e mostre a soma entre os mesmos
+print('Vamos realizar uma soma, escolha dois números')
+num1= input(int)
+num2= input(int)
+resultado = num1 + num2
+print(resultado)
+
+#Incompleta, será refeita no mesmo dia mais tarde.
