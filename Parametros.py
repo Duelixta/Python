35cba656cfdd7309432c3968bde8418a037fc027
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-01-30 00:56:46 -0300

    Update Parametros.py

--- Parametros.py
+++ Parametros.py
@@ -4,3 +4,8 @@ print(7+4)
 print('7'+ '4')
 print('Olá', 5)
 #Todos esses prints tem diferenças, como por exemplo o primeiro print que é uma string, o segundo é uma soma, o terceiro é uma concatenação e o quarto é uma concatenação de uma string com um número.
+#Temos a possibilidade do INPUT, que é uma função que pede um valor ao usuário.
+nome = input('Qual é o seu nome? ')
+idade= input('Qual é a sua idade? ')
+peso= input('Qual é o seu peso? ')
+print(nome, idade, peso)
