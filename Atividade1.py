77fe82c2ab12673c3741d3b7ed2bc2a5e1c1cc38
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-01-30 01:17:29 -0300

    Atividades 1 e 2 sobre PRINT VARIAVEIS E INPUT

---
+++ Atividade1.py
@@ -0,0 +1,4 @@
+#Dia 30/01/2025 atividade numero 1
+#Crie um script de python, que leia o nome de uma pessoa e mostre uma mensagem de boas vindas conforme o valor digitado
+boasvindas = input('Qual é o seu nome? ')
+print('Olá', boasvindas, 'seja bem vindo(a)!')
---
+++ Atividade2.py
@@ -0,0 +1,7 @@
+#Dia 30/01/2025 atividade numero 2
+#Crie um script de python que leia o dia o mês e o ano de nascimento de uma pessoa e mostre tudo como uma data formatada
+nome=input('Qual seu nome?')
+datadenascimento = input('Qual é o dia do seu nascimento? ')
+mesdenascimento = input('Em que mes você nasceu? ')
+anodenascimento = input('Em qual ano você nasceu?')
+print('Olá',nome,',Você nasceu no dia',datadenascimento,'do mês de',mesdenascimento,'em',anodenascimento)
