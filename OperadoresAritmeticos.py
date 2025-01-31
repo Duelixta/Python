7853ade0606392d158d0d9730e8390f0953a6902
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-01-31 04:05:54 -0300

    Create OperadoresAritmeticos.py

---
+++ OperadoresAritmeticos.py
@@ -0,0 +1,17 @@
+#Adição +
+#Subtração -
+#Multiplicação *
+#Divisão /
+#Potencia **
+#Divisao inteira //
+#Resto da divisao %
+B = 5
+C = 2
+A = B + C
+D = B - C
+E = B * C
+F = B/C
+G = B**C
+H = B//C
+I = B%c
+
