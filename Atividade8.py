85aa23cb1f98848bea97804b4934c7f6fc579876
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-03 11:48:20 -0300

    Atvd8

---
+++ Atividade8.py
@@ -0,0 +1,9 @@
+#Crie um programa que leia o dobro de um numero, seu triplo e sua raiz quadrada
+
+numero = int(input('Me de um numero'))
+
+Dobro = numero * 2
+Triplo = numero * 3
+Raiz = numero ** (1/2)
+
+print('Seu dobro eh {}, seu triplo eh {} e sua raiz eh {}'.format(Dobro,Triplo,Raiz))
