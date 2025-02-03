638c2aa20edaebcc5efc4e7c13d6385f63d3c0a7
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-03 11:42:05 -0300

    Extra

--- Extras1.py
+++ Extras1.py
@@ -1,2 +1,16 @@
-nome = input('Olá qual seu nome')
-print('Prazer {:X^20}, Boa tarde'.format(nome))
+
+#Botar tags nas que vc n usa
+#nome = input('Olá qual seu nome')
+#print('Prazer {:X^20}, Boa tarde'.format(nome))
+n1 = int(input('Me de um numero'))
+n2 = int(input('Me de outro numero'))
+s = n1 + n2 
+sub = n1 - n2
+m = n1 * n2
+e = n1 ** n2
+d = n1/n2 
+di = n1//n2
+
+print('Sua soma eh {}\n, a subtracao eh {}\n, a multiplicacao eh {}\n, a exponenciacao eh {:3}\n'.format(s,sub,m,e), end= '\nmacacada\n')
+print('Sua divisao eh {:.3f}\n, e sua divisao inteira eh {}\n'.format(d,di))
+        
