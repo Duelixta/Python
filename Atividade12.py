5132d1a3b072ab036a528703e8d01ca9ba4c8003
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-03 12:12:17 -0300

    Atvd12

---
+++ Atividade12.py
@@ -0,0 +1,7 @@
+#Leia quantos reais a pessoa tem na carteira e mostre quantos dolares ela pode comprar 1USD = 3,27RS
+
+dinheiro = float(input('Quanto dinheiro voce tem?'))
+
+convertido = dinheiro/3.27
+
+print('Voce pode comprar {} dolares'.format(convertido))
