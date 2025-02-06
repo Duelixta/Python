99ec72124e39c268da6dc4edb2317ee8ced98021
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-06 06:59:41 -0300

    Update Modulos.py

--- Modulos.py
+++ Modulos.py
@@ -2,3 +2,7 @@
 #Colocamos from(nome da biblioteca) import Item da biblioteca para importarmos algo especifico
 
 #Biblioteca MATH, CEIL (Arredonda pra cima) FLOOR(Arredonda pra baixo), TRUNCAR(ELIMINAR DA VIRGULA PRA FRENTE), POW (POTENCIA), SQRT (RAIZ QUADRADA), FACTORIAL(FATORIAIS)
+import math
+num = int(input('Me de um numero'))
+raiz = math.sqrt(num)
+print('Seu numero e {}'.format(raiz))
