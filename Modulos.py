404d94af60eb60386d85ac1431ba66ce350a7d20
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-06 06:54:30 -0300

    Create Modulos.py

---
+++ Modulos.py
@@ -0,0 +1,4 @@
+#Comando import, importar algo de uma biblioteca
+#Colocamos from(nome da biblioteca) import Item da biblioteca para importarmos algo especifico
+
+#Biblioteca MATH, CEIL (Arredonda pra cima) FLOOR(Arredonda pra baixo), TRUNCAR(ELIMINAR DA VIRGULA PRA FRENTE), POW (POTENCIA), SQRT (RAIZ QUADRADA), FACTORIAL(FATORIAIS)
