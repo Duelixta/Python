d650db316e461c0aa28d3b20edac565bec7e3718
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-11 03:05:36 -0300

    ATVBD 16

---
+++ Atividade16.py
@@ -0,0 +1,4 @@
+#Crie um progrmaa que leia um numero real e leia sua porcao inteira na tela
+import math
+num = float(input('Me de um numero real'))
+print('seu numero eh {}'.format(math.trunc(num)))
