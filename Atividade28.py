4eb8b252bfd3a319048519aeb6ade3ae7def0425
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-25 06:46:51 -0300

    Create Atividade28.py

---
+++ Atividade28.py
@@ -0,0 +1,15 @@
+#Faça um programa que leia uma frase e mostre
+
+#Quantas vezes aparece a letra A
+
+frase = str(input('Me de uma frase'))
+
+print(frase.count('a'))
+
+#Em qual posição a aparece pela primeira vez 
+
+print(frase.find('a'))
+
+#Em qual posição a aparece pela ultima vez
+
+print(frase.rfind('a'))
