3515a6c803e68762cb256ffddcdf7e68dd1c804a
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-02-25 06:06:25 -0300

    Atividade24

---
+++ Atividade23.py
@@ -0,0 +1,7 @@
+frase = 'Macaco come BANANA e coco'
+print('Macaco' in frase)
+print(frase.lower().find('banana'))
+
+frase2 = 'Macacos unidos vencem'
+listaf2 = frase2.split()
+print(listaf2[1][0])
