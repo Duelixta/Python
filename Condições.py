0b89bb04b530bb18fe9fffcb94ddd35f8efc682e
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-03-03 15:20:08 -0300

    Update Condições.py

--- Condições.py
+++ Condições.py
@@ -1,1 +1,8 @@
 #Estruturas sequênciais
+#IF E ELSE
+nome = str(input('Qual seu nome'))
+if nome == 'Gustavo': #Aqui não é composta ainda
+    print('Que nome bonito!')
+else: #Aqui já é composta
+    print('Nome de macaco')
+print('Bom dia, senhor(a){}'.format(nome))
