1a8998e7f39fd39cfceca0022845f4c778f629e5
Gustavo Henrique Montanari <gustavo.montanari22@gmail.com>
2025-03-04 12:53:59 -0300

    Update Condições.py

--- Condições.py
+++ Condições.py
@@ -6,3 +6,17 @@ if nome == 'Gustavo': #Aqui não é composta ainda
 else: #Aqui já é composta
     print('Nome de macaco')
 print('Bom dia, senhor(a){}'.format(nome))
+
+#Outro exemplo
+
+n1 = float(input('Insira uma nota'))
+n2 = float(input('Insira uma nota'))
+
+m2 = (n1 + n2)/2
+
+print('Sua média foi de {}'.format(m2))
+
+if m2 > 5.0:
+    print('Você foi aprovado!!')
+else:
+    print('Reprovado.')
